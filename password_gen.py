37079aa3b26da091926c38a3146d8dd594da4d8b
Jackson Dockery <jhdocker@gmail.com>
2022-01-29 17:57:28 -0500

    adding an option for multi-char or just alpha-numeric

--- password_gen.py
+++ password_gen.py
@@ -4,20 +4,31 @@ import argparse
 import string
 import random
 
-def random_char(length):
+def arg_check(pw_type):
+	available_types = ['alpha-numeric', 'multi-char', '']
+	if pw_type in available_types:
+		return True
+	else:
+		print(f"{pw_type} is not a valid option. Please choose an available -type from the help list.")
+		return False
+
+# this is a silly way to add the various characters
+# need to rewrite
+def random_char(length, type):
 	x = 0
 	password = []
 	while x < (length * 3):
 		password.append(random.choice(string.ascii_letters))
-		password.append(random.choice(string.ascii_letters).upper())
 		password.append(str(random.randint(0,9)))
+		if type == 'multi-char':
+			password.append(random.choice(string.punctuation))
 		x += 1
 
 	return password
 
 
-def random_pass(length):
-	long_pass = random_char(length)
+def random_pass(length, type):
+	long_pass = random_char(length, type)
 	while len(long_pass) > length:
 		long_pass.pop()
 		random.shuffle(long_pass)
@@ -27,10 +38,18 @@ def random_pass(length):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-length', type=int, required=True, help="this is the help dialogue")
+parser.add_argument('-type', type=str, required=False, default='',help="""use the following arguments 
+	to build different character passwords: alpha-numeric, multi-char""")
 args = parser.parse_args()
+# available_types = ['alpha-numeric', 'multi-char', '']
+# if args.type in available_types:
+# 	print(f'{args.type} is in there NEWWWW yep its good')
+# else:
+# 	print('nope, not in ther jackson')
+	
+if arg_check(args.type):
+	print(random_pass(args.length, args.type))
+else:
+	print('closing script')
 
-if args.length:
-	pass_len = args.length
-
-print(random_pass(args.length))
 
